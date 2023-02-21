d8a41aa6613784f06847f957bda8a83df74422be
devevandro <dev.evandro@gmail.com>
2023-02-20 23:27:52 -0300

    remove packages

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-import express from 'express';
-import cors from 'cors';
 import jsonServer from 'json-server';
 
 const port = 4500;
-const app = express();
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-
-app.use(cors);
-app.use(express.json());
 
 server.use(middlewares);
 server.use(jsonServer.rewriter({
