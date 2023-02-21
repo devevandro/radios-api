3773004fd12e960eb45c8ce430cebf550c7d9b53
devevandro <dev.evandro@gmail.com>
2023-02-20 23:19:15 -0300

    create config to json-server

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import jsonServer from 'json-server';
 
 const port = 4500;
 const app = express();
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
 app.use(cors);
 app.use(express.json());
 
-app.listen(port, () => {
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1'
+}));
+server.use(router);
+
+server.listen(port, () => {
   console.log(`Server on port ${port}`);
 });
